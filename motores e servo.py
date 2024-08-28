28a76f41eb06cbc52faa1fb990fb9d5d67fa9bfe
IuriBock <iuribock@gmail.com>
2024-08-28 15:07:24 -0300

    Motores e Servos Organizado.py

--- motores e servo.py
+++ motores e servo.py
@@ -15,12 +15,14 @@ LED_PIN = 14
 SERVO_PIN = 13  # Pino gpio para o servo motor
 
 # Inicializar gpio
-gpio.setmode(gpio.BCM)
-gpio.setup(PWM_PIN_A, gpio.OUT)
-gpio.setup(IN1_PIN_A, gpio.OUT)
-gpio.setup(IN2_PIN_A, gpio.OUT)
-gpio.setup(SERVO_PIN, gpio.OUT)
-gpio.setup(LED_PIN, gpio.OUT)
+def setup():
+    gpio.setmode(gpio.BCM)
+    gpio.setup(PWM_PIN_A, gpio.OUT)
+    gpio.setup(IN1_PIN_A, gpio.OUT)
+    gpio.setup(IN2_PIN_A, gpio.OUT)
+    gpio.setup(SERVO_PIN, gpio.OUT)
+    gpio.setup(LED_PIN, gpio.OUT)
+setup()
 
 # Inicializar pigpio para controle de servo
 #pi = pigpio.pi()
@@ -28,53 +30,39 @@ gpio.setup(LED_PIN, gpio.OUT)
 pwm = gpio.PWM(PWM_PIN_A, 1)
 
 def liga_led():
+    setup()
     gpio.output(LED_PIN, gpio.HIGH)
     
 def desliga_led():
+    setup()
     gpio.output(LED_PIN, gpio.LOW)
 
 # FunÃ§Ãµes de controle dos motores
 def motor_forward(pin_pwm, pin_in1, pin_in2):
     gpio.cleanup()  # Limpa a configuracao de todos os pinos
-    print('limpa config')
-    gpio.setmode(gpio.BCM)
-    gpio.setup(PWM_PIN_A, gpio.OUT)
-    gpio.setup(IN1_PIN_A, gpio.OUT)
-    gpio.setup(IN2_PIN_A, gpio.OUT)
-    gpio.setup(PWM_PIN_A, gpio.OUT)
-    print("setup frente ok")
+    setup()
     gpio.output(IN1_PIN_A, gpio.HIGH)
     gpio.output(IN2_PIN_A, gpio.LOW)
-    print('portas frente ok')
     pwm = gpio.PWM(PWM_PIN_A, 1)
     pwm.start(100)
-    print('setou pwm')
     return pwm
 
 def motor_backward(pin_pwm, pin_in1, pin_in2):
     gpio.cleanup()  # Limpa a configuracao de todos os pinos
-    gpio.setmode(gpio.BCM)
-    gpio.setup(PWM_PIN_A, gpio.OUT)
-    gpio.setup(IN1_PIN_A, gpio.OUT)
-    gpio.setup(IN2_PIN_A, gpio.OUT)
-    gpio.setup(PWM_PIN_A, gpio.OUT)
+    setup()
     gpio.output(IN1_PIN_A, gpio.LOW)
     gpio.output(IN2_PIN_A, gpio.HIGH)
     pwm = gpio.PWM(PWM_PIN_A, 1)
     pwm.start(100)
-
     return pwm
 
 def stop_motor(pwm):
     pwm.stop()
     gpio.cleanup()  # Limpa a configuracao de todos os pinos
 
-
 #FunÃ§Ã£o para controlar o servo motor
 def set_servo_angle(duty):
     gpio.cleanup()  # Limpa a configuracao de todos os pinos
-    #duty = (angle / 18) + 2
-    #pi.set_servo_pulsewidth(SERVO_PIN, duty)
     gpio.setmode(gpio.BCM)
     gpio.setup(SERVO_PIN, gpio.OUT)
 
@@ -83,42 +71,6 @@ def set_servo_angle(duty):
     servo.ChangeDutyCycle(duty)  
 
 
-# Exemplo de movimento do carrinho
-try:
-    while True:
-        
-        #pwm_motor_a = motor_backward(PWM_PIN_A, IN1_PIN_A, IN2_PIN_A)
-        #pulso = angle_to_pulsewidth(90)
-        stop_motor(pwm)
-
-
-
-
-
-
-
-
-
-        break
-        set_servo_angle(9)  # PosiÃ§Ã£o central do servo
-        time.sleep(1)
-        set_servo_angle(11)
-        time.sleep(1)
-        #liga_led()
-        #time.sleep(2)  # Movimento para frente por 2 segundos
-        #desliga_led()
-        #stop_motor(pwm)
-
-        
-
-        #time.sleep(1)  # Pausa de 1 segundo
-
-        #pwm_motor_a = motor_forward(PWM_PIN_A, IN1_PIN_A, IN2_PIN_A)
-
-        #time.sleep(2)  # Movimento para trÃ¡s por 2 segundos
-
-        #stop_motor(pwm_motor_a)
-        
 
 except KeyboardInterrupt:
     stop_motor(pwm)
