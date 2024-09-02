d5b0292e6e3805478dcd7f26dfd701324992c448
Iuri <iuribock@gmail.com>
2024-09-02 12:02:21 -0300

    organizando codigos

--- siteRobo/static/scripts/mapascript.js
+++
@@ -1,6 +0,0 @@
-function load_map() { 
-    fetch('/load-map') 
-      .then(response => response.json()) 
-      .then(data => console.log(data)) 
-      .catch(error => console.error(error)); 
-  }
